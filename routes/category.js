39a70689470da290f44b185d117f447e8c67b7d9
Edgar0055 <e.rostomian13@gmail.com>
2019-07-30 19:51:07 +0300

    minor fixes

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const passport = require('passport')
+const upload = require('../middleware/upload')
 const router = express.Router()
 const controller = require('../controllers/category')
-const passport = require('passport')
+
 
 router.get('/', passport.authenticate('jwt', { session: false }), controller.getAll)
 router.get('/:id', controller.getById)
 router.delete('/:id', controller.remove)
-router.post('/', controller.create)
-router.patch('/:id', controller.update)
+router.post('/', upload.single('image'), controller.create)
+router.patch('/:id', upload.single('image'), controller.update)
 
 module.exports = router
